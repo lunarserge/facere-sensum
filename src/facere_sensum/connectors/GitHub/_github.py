ea0f59a42affa8ec57299a069b3c97e3867b46d0
Serge Lunev <2530048+lunarserge@users.noreply.github.com>
2024-08-13 09:25:15 -0700

    Start enabling Black code format checker in CI - just one source file for now (#49)

--- src/facere_sensum/connectors/GitHub/_github.py
+++ src/facere_sensum/connectors/GitHub/_github.py
@@ -1,23 +1,24 @@
 # SPDX-License-Identifier: MIT
 
-'''
-Common functionality for GitHub data connectors.
-'''
+"""
+Common functionality for GitHub metric sources.
+"""
 
 import github
 from facere_sensum import fs
 
-_token = fs.get_3rd_party_entry('GitHub', 'personal access token')
-g = github.Github(auth = github.Auth.Token(_token) if _token else None)
+_token = fs.get_3rd_party_entry("GitHub", "personal access token")
+g = github.Github(auth=github.Auth.Token(_token) if _token else None)
+
 
 def get_normalized(metric, raw, target):
-    '''
-    Get standard (i.e., normalized) metric score for a GitHub metric.
-    'metric' is the metric JSON description.
+    """
+    Get normalized metric score for a GitHub metric.
+    'metric' is the metric definition.
     'raw' is the raw metric score.
     'target' is the success target for the normalized metric to hit 0.5 mark.
-    '''
-    if 'target' in metric:
-        target = metric['target']
+    """
+    if "target" in metric:
+        target = metric["target"]
     perfect = target * 2
-    return 1 if raw >= perfect else raw/perfect
+    return 1 if raw >= perfect else raw / perfect
