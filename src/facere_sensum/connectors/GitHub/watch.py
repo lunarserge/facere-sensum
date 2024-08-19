eea9fbd6f987a594556d26fb496358e0e5743670
Serge Lunev <lunarserge@users.noreply.github.com>
2024-08-19 16:20:00 -0700

    enable source formatting for everything in src/facere_sensum/connectors/GitHub folder

--- src/facere_sensum/connectors/GitHub/watch.py
+++ src/facere_sensum/connectors/GitHub/watch.py
@@ -1,25 +1,29 @@
 # SPDX-License-Identifier: MIT
 
-'''
-Data connector for number of GitHub watchers.
-'''
+"""
+Metric source for GitHub watchers.
+"""
 
 from . import _github as gh
 
 # Default success target for GutHub watchers.
 _TARGET = 50
 
-def get_raw(metric): # pragma: no cover - can't test REST API response changing over time.
-    '''
+
+def get_raw(
+    metric,
+):  # pragma: no cover - can't test REST API response changing over time.
+    """
     Get raw metric score for GitHub watchers: their number.
-    'metric' is the metric JSON description.
-    '''
-    return gh.g.get_repo(metric['repo']).subscribers_count
+    'metric' is the metric definition.
+    """
+    return gh.g.get_repo(metric["repo"]).subscribers_count
+
 
 def get_normalized(metric, raw):
-    '''
-    Get standard (i.e., normalized) metric score for GitHub watchers.
-    'metric' is the metric JSON description.
+    """
+    Get normalized metric score for GitHub watchers.
+    'metric' is the metric definition.
     'raw' is the raw metric score.
-    '''
-    return gh.get_normalized(metric,raw,_TARGET)
+    """
+    return gh.get_normalized(metric, raw, _TARGET)
