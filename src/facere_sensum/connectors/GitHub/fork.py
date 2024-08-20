7b95bf3c475f12be6469f55430f40327e3dd839b
Serge Lunev <2530048+lunarserge@users.noreply.github.com>
2024-08-20 14:46:58 -0700

    enable source formatting for everything in src/facere_sensum/connectors/GitHub folder (#52)
    Co-authored-by: Serge Lunev <lunarserge@users.noreply.github.com>

--- src/facere_sensum/connectors/GitHub/fork.py
+++ src/facere_sensum/connectors/GitHub/fork.py
@@ -1,25 +1,29 @@
 # SPDX-License-Identifier: MIT
 
-'''
-Data connector for GitHub forks.
-'''
+"""
+Metric source for GitHub forks.
+"""
 
 from . import _github as gh
 
 # Default success target for GutHub forks.
 _TARGET = 100
 
-def get_raw(metric): # pragma: no cover - can't test REST API response changing over time.
-    '''
+
+def get_raw(
+    metric,
+):  # pragma: no cover - can't test REST API response changing over time.
+    """
     Get raw metric score for GitHub forks: their number.
-    'metric' is the metric JSON description.
-    '''
-    return gh.g.get_repo(metric['repo']).forks_count
+    'metric' is the metric definition.
+    """
+    return gh.g.get_repo(metric["repo"]).forks_count
+
 
 def get_normalized(metric, raw):
-    '''
-    Get standard (i.e., normalized) metric score for GitHub forks.
-    'metric' is the metric JSON description.
+    """
+    Get normalized metric score for GitHub forks.
+    'metric' is the metric definition.
     'raw' is the raw metric score.
-    '''
-    return gh.get_normalized(metric,raw,_TARGET)
+    """
+    return gh.get_normalized(metric, raw, _TARGET)
