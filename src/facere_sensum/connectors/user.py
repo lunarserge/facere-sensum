883519645c8cb151e15b9dd7ded2ebd42dcb3034
Lunev <lunarserge@users.noreply.github.com>
2024-09-10 14:34:39 -0700

    Enable source formatting for everything under src/facere_sensum/connectors

--- src/facere_sensum/connectors/user.py
+++ src/facere_sensum/connectors/user.py
@@ -1,25 +1,30 @@
 # SPDX-License-Identifier: MIT
 
-'''
-Data connector for direct user input.
-'''
+"""
+Metric source for direct user input.
+"""
 
-def get_raw(metric): # pragma: no cover - can't automatically test direct user input
-    '''
-    Get metric score as a direct user input.
-    'metric' is the metric JSON description.
-    '''
-    metric = metric['id']
+
+def get_raw(metric):  # pragma: no cover - can't automatically test direct user input
+    """
+    Get raw metric score for direct user input.
+    'metric' is the metric definition.
+    """
+    metric = metric["id"]
     while True:
         score = float(input(f"Enter score for '{metric}': "))
         if 0 <= score <= 1:
             return score
-        print('Entered score is outside 0..1 range, enter again')
+        print("Entered score is outside 0..1 range, enter again")
 
-def get_normalized(metric, raw): # pylint: disable=unused-argument
-    '''
-    Get standard (i.e., normalized) metric score for a direct user input. It is the same as raw.
-    'metric' is the metric JSON description.
+
+def get_normalized(
+    metric,  # pylint: disable=unused-argument
+    raw,
+):
+    """
+    Get normalized metric score for direct user input. It is the same as raw.
+    'metric' is the metric definition.
     'raw' is the raw metric score.
-    '''
+    """
     return raw
