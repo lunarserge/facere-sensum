44740def77b83b1f028e5dbf7cc140be4b6e53fd
Serge Lunev <2530048+lunarserge@users.noreply.github.com>
2025-01-15 15:18:30 -0800

    add remaining Python files into source formatting / linting / security check (#61)
    Co-authored-by: Lunev <lunarserge@users.noreply.github.com>

--- doc/conf.py
+++ doc/conf.py
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: MIT
 
-'''
+"""
 Configuration file for the Sphinx documentation builder.
-'''
+"""
 
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -10,20 +10,20 @@ Configuration file for the Sphinx documentation builder.
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'facere-sensum'
-copyright = '2023, Serge Lunev'
-author = 'Serge Lunev'
+project = "facere-sensum"  # pylint: disable=invalid-name
+copyright = "2023-2025 Serge Lunev. All Rights Reserved."  # pylint: disable=invalid-name,redefined-builtin
+author = "Serge Lunev"  # pylint: disable=invalid-name
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"  # pylint: disable=invalid-name
+html_static_path = ["_static"]
