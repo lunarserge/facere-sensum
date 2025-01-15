533d0f6133544772daf945de19d01af8f56d5409
Lunev <lunarserge@users.noreply.github.com>
2025-01-15 14:52:15 -0800

    add remaining Python files into source formatting / linting / security check

--- fsy.py
+++ fsy.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: MIT
 
-'''
+"""
 facere-sensum debug launcher.
 Need to keep this separate to make sure fs.py is imported as a module and
 not used as the main script.
-'''
+"""
 
 from facere_sensum import fs
 
